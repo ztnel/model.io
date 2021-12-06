d43b5f7d457dfb68d68a4aed35c65228953514cf
cSDes1gn <christian911@sympatico.ca>
2021-12-06 00:56:07 -0500

    add state model export

--- modelio/__init__.py
+++ modelio/__init__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['__version__', 'BaseModel', '_PKey']
+__all__ = ['__version__', 'BaseModel', 'StateModel', '_PKey']
 
 import logging
 from modelio.__version__ import __version__
 from modelio.typing import _PKey
 from modelio.models.base import BaseModel
+from modelio.models.state import StateModel
 
 _log = logging.getLogger(__name__)
 _log.info("Model.io version %s", __version__)
