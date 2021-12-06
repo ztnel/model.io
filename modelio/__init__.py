ca30b17ff67e8b026bf0819f1c3aba8f4f1ce55f
cSDes1gn <christian911@sympatico.ca>
2021-12-06 00:39:21 -0500

    update imports

--- modelio/__init__.py
+++ modelio/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['__version__']
+__all__ = ['__version__', 'BaseModel', '_PKey']
 
 import logging
 from modelio.__version__ import __version__
+from modelio.typing import _PKey
+from modelio.models.base import BaseModel
 
 _log = logging.getLogger(__name__)
 _log.info("Model.io version %s", __version__)
