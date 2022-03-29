77dcdf415f633d059be0ccae1e3455ac0f594f07
cSDes1gn <christian911@sympatico.ca>
2022-03-28 23:39:40 -0400

    add hash verification mechanism; implement ssm wrapper around state model refs for callback storage

--- myosin/state/state.py
+++ myosin/state/state.py
@@ -15,14 +15,16 @@ from monitor.models.experiment import Experiment
 from monitor.models.imaging_profile import ImagingProfile
 ```
 """
+import asyncio
 import copy
 import logging
-import asyncio
-import traceback
-from typing import Any, Coroutine, Dict, Tuple, Type, TypeVar, Callable, List
+from typing import Dict, Type, TypeVar, Callable
+
+from myosin.state.ssm import SSM
+from myosin.typing import AsyncCallback
 from myosin.utils.funcs import pformat
 from myosin.models.state import StateModel
-from myosin.exceptions.state import NullCheckoutError
+from myosin.exceptions.state import HashNotFound, NullCheckoutError
 from myosin.utils.concurrency import ThreadUtils as tutils
 
 # generic runtime model type
@@ -32,7 +34,7 @@ _T = TypeVar('_T', bound=StateModel)
 class State:
 
     # storage mechanism is { name: Model }
-    _ssm: Dict[int, Any] = {}
+    _ssm: Dict[int, SSM] = {}
 
     def __init__(self) -> None:
         self._logger = logging.getLogger(__name__)
@@ -49,67 +51,57 @@ class State:
         :param model: state model
         :type model: StateModel
         """
-        self._ssm[model.__typehash__()] = model
+        self._ssm[model.__typehash__()] = SSM[_T](model)
         self._logger.info("Loaded state model: %s", pformat(model.serialize()))
         return model
 
     @tutils.lock(tutils.state_lock)
-    def checkout(self, _type: Type[_T]) -> _T:
+    def checkout(self, state_type: Type[_T]) -> _T:
         """
-        Returns requested state model
+        Returns reference of requested state model
 
+        :param state_type: requested state model reference 
+        :type state_type: Type[_T]
+        :raises NullCheckoutError: if the requested state type does not exist 
         :return: deep copy of requested state model
-        :rtype: StateModel
+        :rtype: _T
         """
-        self._logger.info("Checking out state model of type %s", _type)
-        _type_hash = hash(_type)
+        self._logger.info("Checking out state model of type %s", state_type)
+        _type_hash = hash(state_type)
         self._logger.debug("Computed type hash: %s", _type_hash)
-        mdl = self._ssm.get(_type_hash)
-        if not mdl:
+        ssm = self._ssm.get(_type_hash)
+        if not ssm:
             raise NullCheckoutError
-        return copy.deepcopy(mdl)
+        return copy.deepcopy(ssm.ref)
 
     @tutils.lock(tutils.state_lock)
     def commit(self, state: _T, cache: bool = False) -> _T:
+        # automatic type inference by typehash
         _type_hash = hash(type(state))
-        self._ssm[_type_hash] = state
+        # verify typehash
+        ssm = self._ssm.get(_type_hash)
+        if not ssm:
+            self._logger.error("Committed typehash: %s did not match any state model", _type_hash)
+            raise HashNotFound
+        ssm.ref = state
+        asyncio.run(ssm.execute())
         if cache:
             state.cache()
             self._logger.debug("Cached commited state model %s", state)
         return state
 
-    def subscribe(self, state_type: Type[_T], callback: Callable[[_T], Coroutine[Any, Any, None]]) -> None:
+    def subscribe(self, state_type: Type[_T], callback: Callable[[_T], AsyncCallback]) -> None:
         """
         Subscribe an asynchronous state change listener to a designated runtime model
 
-        :param state_type: runtime type to subscribe to
+        :param state_type: model type to subscribe to
         :type state_type: Type[T]
         :param callback: state change listener callback
-        :type callback: Callable[[T], Coroutine[Any, Any, None]]
+        :type callback: Callable[[T], AsyncCallback]
         """
-
-    async def _subscriber_runner(self, state_var: _T,
-                                 registry: List[Callable[[_T], Coroutine[Any, Any, None]]]) -> None:
-        """
-        Async runner that executes all subscriber coroutines with new runtime model state changes.
-
-        :param state_var: pre-validated runtime model to pass to subscribers
-        :type state_var: T
-        :param registry: list of subscriber coroutines
-        :type registry: List[Callable[[T], Awaitable[None]]]
-        """
-        # construct coroutine lists
-        tasks = [subscriber(state_var) for subscriber in registry]
-        # return results from coroutines with exceptions if any
-        results = await asyncio.gather(*tasks, return_exceptions=True)
-        # reformat tasks to display only the name
-        operations = tuple(zip(map(lambda x: x.__qualname__, tasks), results))
-        self._logger.debug("%s Subscriber operations: %s", type(state_var).__name__, operations)
-        # filter by operations which yielded an exception
-        exceptions: List[Tuple[str, Exception]] = list(
-            filter(lambda x: x[1] is not None, operations))
-        for func, exc in exceptions:
-            self._logger.exception("Subscriber function: %s encountered an exception: %s", func,
-                                   "".join(traceback.format_exception(
-                                       etype=type(exc), value=exc, tb=exc.__traceback__
-                                   )))
+        _type_hash = hash(state_type)
+        ssm = self._ssm.get(_type_hash)
+        if not ssm:
+            self._logger.error("Subscribed typehash: %s did not match any state model", _type_hash)
+            raise HashNotFound
+        ssm.queue.append(callback)
