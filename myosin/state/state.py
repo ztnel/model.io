3d1d6a382f932f724dd12c9698b122e281c108ac
ztnel <christian911@sympatico.ca>
2022-05-16 23:35:48 -0400

    update locking mechanism using cm

--- myosin/state/state.py
+++ myosin/state/state.py
@@ -8,13 +8,13 @@ Modified: 2022-04
 import asyncio
 import copy
 import logging
+from threading import Lock
 from typing import Dict, Type, TypeVar, Callable
 
 from myosin.state.ssm import SSM
 from myosin.typing import AsyncCallback
 from myosin.utils.funcs import pformat
 from myosin.models.state import StateModel
-from myosin.utils.concurrency import ThreadUtils as tutils
 from myosin.exceptions.state import HashNotFound, NullCheckoutError, UninitializedStateError
 
 # generic runtime model type
@@ -25,14 +25,17 @@ class State:
 
     # storage mechanism is { name: Model }
     _ssm: Dict[int, SSM] = {}
+    state_lock = Lock()
 
     def __init__(self) -> None:
         self._logger = logging.getLogger(__name__)
 
     def __enter__(self):
+        self.state_lock.acquire()
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb): ...
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.state_lock.release()
 
     def load(self, model: _T) -> _T:
         """
@@ -41,23 +44,22 @@ class State:
         :param model: state model
         :type model: StateModel
         """
-        self._ssm[model.__typehash__()] = SSM[_T](model)
-        # assert the model attributes are initialized
+        model.load()
         try:
             model.serialize()
         except AttributeError as exc:
             raise UninitializedStateError from exc
+        self._ssm[model.__typehash__()] = SSM[_T](model)
         self._logger.info("Loaded state model: %s", pformat(model.serialize()))
         return model
 
-    @tutils.lock(tutils.state_lock)
     def checkout(self, state_type: Type[_T]) -> _T:
         """
-        Returns reference of requested state model
+        Returns a deepcopy of requested state model
 
-        :param state_type: requested state model reference 
+        :param state_type: 
         :type state_type: Type[_T]
-        :raises NullCheckoutError: if the requested state type does not exist 
+        :raises NullCheckoutError: if the requested state type does not exist
         :return: deep copy of requested state model
         :rtype: _T
         """
@@ -69,7 +71,6 @@ class State:
             raise NullCheckoutError
         return copy.deepcopy(ssm.ref)
 
-    @tutils.lock(tutils.state_lock)
     def commit(self, state: _T, cache: bool = False) -> _T:
         """
         Commit new state to system state and update state subscriber callbacks
@@ -78,7 +79,7 @@ class State:
         :type state: _T
         :param cache: cache the state to disk once updated, defaults to False
         :type cache: bool, optional
-        :raises HashNotFound: if system state has no state registered of the requested type 
+        :raises HashNotFound: if system state has no state registered of the requested type
         :return: updated system state reference
         :rtype: _T
         """
