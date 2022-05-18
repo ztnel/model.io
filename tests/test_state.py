35e3b3a0eeae81e5f90a41c3f14f18b097997b58
ztnel <christian911@sympatico.ca>
2022-05-17 22:15:23 -0400

    improve unittest coverage

--- tests/test_state.py
+++ tests/test_state.py
@@ -6,9 +6,11 @@ Modified: 2022-04
 
 """
 
+import asyncio
+from typing import Dict
 import unittest
 import logging
-from unittest.mock import MagicMock, patch
+from unittest.mock import AsyncMock, MagicMock, patch
 from myosin.models.state import StateModel
 from tests.resources.models import DemoState
 
@@ -22,8 +24,7 @@ class TestState(unittest.TestCase):
     def setUp(self) -> None:
         logging.disable()
         self.test_state = DemoState(1)
-        with State() as state:
-            self.state = state
+        self.state = State()
 
     def tearDown(self) -> None:
         logging.disable(logging.NOTSET)
@@ -36,28 +37,36 @@ class TestState(unittest.TestCase):
         """
         self.state._ssm[self.test_state.__typehash__()] = SSM[DemoState](self.test_state)
 
-    def test_uninitialized_load(self):
+    @patch.object(StateModel, 'load')
+    @patch.object(StateModel, 'serialize', side_effect=AttributeError)
+    def test_uninitialized_load(self, _: MagicMock, load: MagicMock):
         """
         Test loading an uninitalized model
         """
         with self.assertRaises(UninitializedStateError):
             self.state.load(self.test_state)
+        load.assert_called_once()
 
     def test_cm_lock(self):
         """
-        Test state lock aquisition and release in context
+        Test context manager state lock aquisition and release
         """
+        # NOTE: threading.Lock methods cannot be mocked
         with State() as state:
             self.assertTrue(state.state_lock.locked())
         self.assertFalse(state.state_lock.locked())
 
-    def test_load(self):
+    @patch.object(StateModel, 'load')
+    @patch.object(DemoState, 'serialize', **{'return_value': "test"})
+    def test_load(self, serialize: MagicMock, load: MagicMock):
         """
         Test state load keyset and SSM wrapper configuration
         """
         # initialize model
         self.test_state.name = "test"
         self.state.load(self.test_state)
+        load.assert_called_once()
+        serialize.assert_called_once()
         # im setting the test like this in order to test both the keyset and the object set,
         # I want to raise test assertion failures rather than key errors.
         ssm = self.state._ssm.get(self.test_state.__typehash__())
@@ -87,13 +96,38 @@ class TestState(unittest.TestCase):
         with self.assertRaises(HashNotFound):
             self.state.commit(self.test_state)
 
-    @patch('asyncio.run', lambda x: None)
-    def test_commit(self):
+    @patch.object(SSM, 'execute', new_callable=AsyncMock)
+    @patch.object(StateModel, 'cache')
+    def test_cached_commit(self, cache: MagicMock, execute: AsyncMock):
         """
-        Test state commit
+        Test state commit with caching
+        """
+        self.mock_loader()
+        self.state.commit(self.test_state, cache=True)
+        execute.assert_awaited_once()
+        cache.assert_called_once()
+
+    @patch.object(SSM, 'execute', new_callable=AsyncMock)
+    @patch.object(StateModel, 'cache')
+    def test_uncached_commit(self, cache: MagicMock, execute: AsyncMock):
+        """
+        Test state commit without caching
         """
         self.mock_loader()
         self.state.commit(self.test_state)
+        execute.assert_awaited_once()
+        cache.assert_not_called()
+
+    @patch.object(asyncio, 'run')
+    def test_commit_hash_not_found(self, run: MagicMock):
+        """
+        Test commit on unregistered state model
+        """
+        mock_ssm = self.mock_ssm(self.state._ssm)
+        mock_ssm.get.return_value = None
+        with self.assertRaises(HashNotFound):
+            self.state.commit(self.test_state)
+        run.assert_not_called()
 
     @patch.object(StateModel, 'clear')
     def test_clear(self, clear: MagicMock):
@@ -103,3 +137,9 @@ class TestState(unittest.TestCase):
         self.mock_loader()
         self.state.reset()
         clear.assert_called_once()
+
+    @staticmethod
+    def mock_ssm(ssm: Dict) -> MagicMock:
+        mm = MagicMock()
+        mm.__getitem__.side_effect = ssm.__getitem__
+        return mm
