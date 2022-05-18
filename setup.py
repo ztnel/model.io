d17af639b56f9bbab4608b9109dc3076f919fcee
ztnel <christian911@sympatico.ca>
2022-05-17 23:06:28 -0400

    add python version trove classifiers and fix license

--- setup.py
+++ setup.py
@@ -55,6 +55,10 @@ setup(
         'Operating System :: Unix',
         'Topic :: Software Development :: Object Brokering',
         'Topic :: Software Development :: Embedded Systems',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.10'
+        'License :: OSI Approved :: MIT License',
     ],
 )
