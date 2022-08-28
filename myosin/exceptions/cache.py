e68544cb516eee8df6d8bcfe054160b9f46563ba
ztnel <christian911@sympatico.ca>
2022-08-28 01:30:14 -0400

    improve exception docstrings

--- myosin/exceptions/cache.py
+++ myosin/exceptions/cache.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-State Exceptions
-=====================
-Modified: 2021-11
+Model Caching Exceptions
+========================
 """
 
 
 class CacheException(Exception):
+    """
+    General model cache exception.
+    """
+
     def __init__(self, msg: str = "Cache encountered a general exception") -> None:
         self.message = msg
 
 
 class CachePathError(CacheException):
-    pass
+    """
+    Raised on failure resolving caching path.
+    """
 
 
 class NullCachePathError(CacheException):
-    pass
+    """
+    Raised on unset caching basepath. Basepath can be specified with the ``MYOSIN_CACHE_BASE_PATH``
+    """
