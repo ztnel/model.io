8e0ae3950918afb82d05ffb305fd03846df01463
ztnel <christian911@sympatico.ca>
2022-08-28 00:32:48 -0400

    export metadata on import

--- myosin/__init__.py
+++ myosin/__init__.py
@@ -13,6 +13,7 @@ from myosin.state import State
 from myosin.__version__ import __version__
 from myosin.models.base import BaseModel
 from myosin.models.state import StateModel
+from myosin.utils.metrics import Metrics as metrics
 
 __all__ = [
     '__version__',
@@ -23,3 +24,5 @@ __all__ = [
 
 _log = logging.getLogger(__name__)
 _log.info("Myosin version %s", __version__)
+# export version to prometheus
+metrics.meta.info({'version': __version__})
